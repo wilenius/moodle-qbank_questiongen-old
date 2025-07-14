0be2b38c4367e6cd3d0008d3833e2d04cbc4186e
Philipp Memmel <philipp.memmel@gmail.com>
2025-07-14 20:18:55 +0000

    MBS-10218: Purpose usage info

--- db/hooks.php
+++ db/hooks.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,23 +12,22 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Plugin version and other meta-data are defined here.
+ * Hook listener callbacks.
  *
- * @package     qbank_questiongen
- * @copyright   2023 Ruthy Salomon <ruthy.salomon@gmail.com> , Yedidia Klein <yedidia@openapp.co.il>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    qbank_questiongen
+ * @copyright  2025 ISB Bayern
+ * @author     Philipp Memmel
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'qbank_questiongen';
-$plugin->release = '2.0';
-$plugin->version  = 2025071200;
-$plugin->requires = 2025041400;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->dependencies = [
-        'local_ai_manager' => 2025071200,
+$callbacks = [
+        [
+                'hook' => \local_ai_manager\hook\purpose_usage::class,
+                'callback' => \qbank_questiongen\local\hook_callbacks::class . '::handle_purpose_usage',
+        ],
 ];
